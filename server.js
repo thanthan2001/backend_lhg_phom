46539288dc0e8180155af6226b5fdc9e197bb661
thanthan2001 <thanthan101101@gmail.com>
2025-05-22 17:33:37 +0700

    nhieu qua

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
 const routes = require("./src/index.routes");
 
 app.use(express.urlencoded({ extended: true }));
@@ -9,6 +10,7 @@ app.use(express.json());
 
 // Routes
 routes(app);
+app.use(cors());
 
 app.listen(8989, "0.0.0.0", () => {
   console.log("Server running on port 8989");
